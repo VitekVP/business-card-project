5020392cc6f7991d3c568f20f8521ff2bf268362
Viktor Pochynok <viktor83pochynok@gmail.com>
2026-02-08 18:53:29 +0200

    fix naviganion in mobil menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -2,15 +2,21 @@ const menuOpenBtnEl = document.querySelector('.js-open-menu');
 const menuCloseBtnEl = document.querySelector('.js-close-menu');
 const mobileMenuEl = document.querySelector('.burger-container');
 const bodyEl = document.body;
-
-// console.log(menuOpenBtnEl);
-// console.log(menuCloseBtnEl);
-// console.log(mobileMenuEl);
+const mobileMenuLinks = mobileMenuEl.querySelectorAll('.burger-list-link');
 
 function toggleMenu() {
   mobileMenuEl.classList.toggle('is-open');
   bodyEl.classList.toggle('no-scroll');
 }
 
+function removeMobilMenu() {
+  mobileMenuEl.classList.remove('is-open');
+  bodyEl.classList.remove('no-scroll');
+}
+
 menuOpenBtnEl.addEventListener('click', toggleMenu);
 menuCloseBtnEl.addEventListener('click', toggleMenu);
+
+mobileMenuLinks.forEach(link => {
+  link.addEventListener('click', removeMobilMenu);
+});
